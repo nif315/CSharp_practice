54d69c95955fb8a2e934f6533240e564ca162684
Vladimir Krygin <vkrygin.craftgs@gmail.com>
2019-04-10 13:44:16 +0300

    Square digits kata uploaded

---
+++ Codewars/Square every digit of an int number/Square every digit of a number/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Square_every_digit_of_a_number
+{
+    class Program
+    {
+        public static int SquareDigits(int n)
+        {
+            var listArr = new List<int>();
+            while (n > 0)
+            {
+                listArr.Add(n % 10);
+                n /= 10;
+            }
+            listArr.Reverse();
+
+            int[] array = listArr.ToArray();
+            for (int i=0; i < array.Length; i++)
+            {
+                array[i] = array[i] * array[i];
+            }
+            Int32.TryParse(String.Join("", array), out n);
+            return n;
+        }
+        static void Main(string[] args)
+        {
+            int n = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Result of squaring evey digit is " + SquareDigits(n));
+            Console.ReadKey();
+        }
+    }
+}
