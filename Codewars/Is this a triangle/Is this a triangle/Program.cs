1589b27d3f80c6d53f1b1844fb600a551b1c9d2a
Vladimir Krygin <vkrygin.craftgs@gmail.com>
2019-04-17 16:45:28 +0300

    Is this triangle kata uploaded

---
+++ Codewars/Is this a triangle/Is this a triangle/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Is_this_a_triangle
+{
+    class Program
+    {
+        public static bool IsTriangle(int a, int b, int c)
+        {
+            if (a + b > c && b + c > a && a + c > b)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input length of the side A");
+            int a = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Input length of the side B");
+            int b = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Input length of the side C");
+            int c = Int32.Parse(Console.ReadLine());
+            Console.WriteLine(IsTriangle(a, b, c));
+            Console.ReadKey();
+        }
+    }
+}
