2e79015128ab2a494404755236cc48018953e67f
Vladimir Krygin <vkrygin.craftgs@gmail.com>
2019-04-19 17:12:09 +0300

    String ends with? kata uploaded

---
+++ Codewars/String ends with/String ends with/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_ends_with
+{
+    class Program
+    {
+        public static bool Solution(string str, string ending)
+        { 
+            if (str.EndsWith(ending))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            bool contains;
+            Console.WriteLine("Input any word:");
+            string str = Console.ReadLine();
+            Console.WriteLine("Input word's ending");
+            string ending = Console.ReadLine();
+            Console.WriteLine(Solution(str,ending));
+            Console.ReadKey();
+        }
+    }
+}
