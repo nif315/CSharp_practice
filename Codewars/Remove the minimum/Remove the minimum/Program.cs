b3eed28ee9c7b6eca9cce1a98d24c20471d3b3d3
Vladimir Krygin <vkrygin.craftgs@gmail.com>
2019-04-26 18:13:10 +0300

    Remove the minimum kata uploaded

---
+++ Codewars/Remove the minimum/Remove the minimum/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Remove_the_minimum
+{
+    class Program
+    {
+        public static List<int> RemoveSmallest(List<int> numbers)
+        {
+            if (numbers.Count == 0)
+                return new List<int>();
+            numbers.Remove(numbers.Min());
+            return numbers;
+        }
+
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            int[] numbersInput = new int[5];
+            Console.WriteLine("Input numbers");
+            for (int i = 0; i < numbersInput.Length; i++)
+            {
+                string str = Console.ReadLine();
+                if (str == null || str.Length == 0)
+                    continue;
+                numbersInput[i] = int.Parse(str);
+            }
+            numbers.AddRange(numbersInput);
+            Console.Clear();
+            Console.WriteLine("Your numbers list:");
+            Console.WriteLine(string.Join(",", numbers));
+            Console.WriteLine("New list with the smallest number removed:");
+            Console.WriteLine(string.Join(",", RemoveSmallest(numbers)));
+            Console.ReadKey();
+        }
+    }
+}
