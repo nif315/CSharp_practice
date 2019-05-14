d68d514d8aa787e15583629aa4b756bdd3efff7f
Vladimir Krygin <vkrygin.craftgs@gmail.com>
2019-05-14 15:34:16 +0300

    Not very secure kata uploaded

---
+++ Codewars/Not very secure/Not very secure/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace Not_very_secure
+{
+    class Program
+    {
+        public static bool Alphanumeric(string str)
+        {
+            if (str.All(Char.IsLetterOrDigit) && str.Length > 0)
+                return true;
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input any alphanumeric string:");
+            string str = Console.ReadLine();
+            Console.WriteLine(Alphanumeric(str));
+            Console.ReadKey();
+        }
+    }
+}
