3562ec46c2bedf8d09a2854240a14e19141b53f5
Vladimir Krygin <vkrygin.craftgs@gmail.com>
2019-05-14 11:10:37 +0300

    Where my anagrams at? Kata uploaded

---
+++ Codewars/Where my anagrams at/Where my anagrams at/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Where_my_anagrams_at
+{
+    class Program
+    {
+        public static List<string> Anagrams(string word, List<string> words)
+        {
+            List<string> anagramsList = new List<string>();
+            string[] anagrams = words.ToArray();
+            for (int i = 0; i < anagrams.Length; i++)
+            {
+                char[] char1 = word.ToLower().ToCharArray();
+                char[] char2 = anagrams[i].ToLower().ToCharArray(); 
+                Array.Sort(char1);
+                Array.Sort(char2);
+                string givenWord = new string(char1);
+                string anagram = new string(char2);
+                if (anagram == givenWord)
+                {
+                    anagramsList.Add(anagrams[i]);
+                }
+            }
+            return anagramsList;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input the word:");
+            string word = Console.ReadLine();
+            Console.WriteLine("Input anagrams:");
+            string[] anagrams = new string[5];
+            List<string> words = new List<string>();
+            for (int i = 0; i < anagrams.Length; i++)
+            {
+                anagrams[i] = Console.ReadLine();
+                words.Add(anagrams[i]);
+            }
+            //Console.WriteLine(string.Join(",", words));
+            Console.WriteLine(string.Join(",",Anagrams(word, words)));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Codewars/Where my anagrams at/Where my anagrams at/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Where my anagrams at")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Where my anagrams at")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("95904e5d-cc80-4ae0-bdf4-148c7ec52e77")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
