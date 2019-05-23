ef3b0e5c5b388caef66bcc7170bdeff355f9351e
Vladimir Krygin <vkrygin.craftgs@gmail.com>
2019-05-23 16:42:32 +0300

    int32 to IPv4 kata uploaded

---
+++ Codewars/int32 to IPv4/int32 to IPv4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net;
+
+namespace int32_to_IPv4
+{
+    class Program
+    {
+        public static string UInt32ToIP(uint ip)
+        {
+            return IPAddress.Parse(ip.ToString()).ToString();
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input int32 value:");
+            uint ip = uint.Parse(Console.ReadLine());
+            Console.WriteLine(UInt32ToIP(ip));
+            Console.ReadKey();
+        }
+    }
+}
