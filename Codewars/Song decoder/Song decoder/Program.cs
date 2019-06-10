ac9b34903682222156c4100f4e0f0bb63bac94e7
Vladimir Krygin <vkrygin.craftgs@gmail.com>
2019-06-10 18:30:49 +0300

    Song decoder kata uploaded

---
+++ Codewars/Song decoder/Song decoder/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace Song_decoder
+{
+    class Program
+    {
+        public static string SongDecoder(string input)
+        {
+            string[] decoded = input.Split(new[] { "WUB" }, StringSplitOptions.None);//удаляем все WUB из строки
+            string originalSong = "";
+            for (int i = 0; i < decoded.Length; i++)
+            {
+                if (decoded[i] != "")
+                {
+                    originalSong = originalSong + decoded[i] + " ";
+                }
+            }
+            return originalSong.Trim();//удаляем лишний пробел в конце
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input dubsted string:");
+            string input = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine(SongDecoder(input));
+            Console.ReadKey();
+        }
+    }
+}
