208d42e417b3343d0021a17182d424038e1b312a
Vladimir Krygin <vkrygin.craftgs@gmail.com>
2019-07-05 13:52:09 +0300

    Next bigger number with the same digits kata in progress

---
+++ Codewars/Next bigger number with the same digits/Next bigger number with the same digits/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Next_bigger_number_with_the_same_digits
+{
+    class Program
+    {
+        public static long NextBiggerNumber(long n)
+        {
+            var combo = n.ToString().ToArray();
+
+            //find the pivot
+            int pivot = 0;
+            for (int i = combo.Length - 1; i > 0; i--)
+            {
+                if (combo[i] > combo[i - 1])
+                {
+                    pivot = i - 1;
+                    Console.WriteLine(pivot);
+                    break;
+                }
+            }
+
+            //find the smallest number to the right of pivot
+            List<int> right = new List<int>();
+            for (int i = pivot; i < combo.Length; i++)
+            {
+                right.Add(combo[i]);
+            }
+            //List<int> right = n.ToString().ToList(combo.Length - pivot, combo.Length - pivot);
+            int min = right.Min();
+            Console.WriteLine(min);
+            /*for (int i = 0; i < combo.Length; i++)
+            {
+                if (combo.Length = 1 || )
+                {
+                    return -1;
+                }
+            }*/
+            return n;
+            //code me
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input number to get next bigger number:");
+            long n = long.Parse(Console.ReadLine());
+            Console.WriteLine("Next bigger number is:");
+            //Console.WriteLine(string.Join(",", temp));
+            Console.WriteLine(NextBiggerNumber(n));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Codewars/Next bigger number with the same digits/Next bigger number with the same digits/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Next bigger number with the same digits")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Next bigger number with the same digits")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("d457a158-0fe4-4f91-8a88-1adee1eeea28")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
