29ddf4c57c82020fd3b5a193fcc74565d2790605
Vladimir Krygin <nif315@gmail.com>
2019-07-08 00:53:16 +0300

    Next bigger number kata completed

--- Codewars/Next bigger number with the same digits/Next bigger number with the same digits/Program.cs
+++ Codewars/Next bigger number with the same digits/Next bigger number with the same digits/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Next_bigger_number_with_the_same_digits
 {
@@ -13,17 +11,17 @@ namespace Next_bigger_number_with_the_same_digits
             var combo = n.ToString().ToArray();
 
             //find the pivot
+            int pivotNumber = 0;
             int pivot = 0;
             for (int i = combo.Length - 1; i > 0; i--)
             {
                 if (combo[i] > combo[i - 1])
                 {
                     pivot = i - 1;
-                    Console.WriteLine(pivot);
+                    pivotNumber = combo[pivot];
                     break;
                 }
             }
-
             // if we are unable to find the pivot, skip
             if (pivot == -1)
                 return -1;
@@ -33,20 +31,36 @@ namespace Next_bigger_number_with_the_same_digits
 
             //find the smallest number to the right of pivot
             //Converting to int, because .min() method returns in ASCII format
-            int min = Convert.ToInt32(Convert.ToString(n.ToString().Substring(pivot + 1).Min()));
+            int min = Convert.ToInt32(Convert.ToString(n.ToString().Substring(pivot + 1).Where(f => f > pivotNumber).Min()));
 
-            char swap;
-            for (int i = 0; i < right.Length; i++)
+            int swap;
+            List<int> swapMin = right.Select(x => Convert.ToInt32(x.ToString())).ToList();
+            for (int i = 0; i < swapMin.Count; i++)
             {
-                if (right[i] == min)
+                if (swapMin[i] == min)
                 {
-                    swap = right[0];
-                    right[0] = Convert.ToChar(min);
-                    right[i] = swap;
+                    swap = swapMin[0];
+                    swapMin[0] = min;
+                    swapMin[i] = swap;
 
                 }
             }
-            return n;
+
+            List<int> sortedRight = new List<int>();
+            for (int i = 1; i < swapMin.Count; i++)
+            {
+                sortedRight.Add(swapMin[i]);
+            }
+            sortedRight.Sort((a, b) => a.CompareTo(b));
+            sortedRight.Insert(0, min);
+
+            string left = n.ToString().Substring(0, pivot);
+            string result = left + String.Join("", sortedRight);
+
+            if (long.Parse(result) < n || long.Parse(result) == n)
+             return -1;
+
+            return long.Parse(result);
         }
 
         static void Main(string[] args)
