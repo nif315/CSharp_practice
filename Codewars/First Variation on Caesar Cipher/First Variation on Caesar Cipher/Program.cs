585adc09393736233165e61600ea1a89f361419a
Vladimir Krygin <nif315@gmail.com>
2019-08-04 22:50:00 +0300

    Demoving shift

--- Codewars/First Variation on Caesar Cipher/First Variation on Caesar Cipher/Program.cs
+++ Codewars/First Variation on Caesar Cipher/First Variation on Caesar Cipher/Program.cs
@@ -34,7 +34,7 @@ namespace First_Variation_on_Caesar_Cipher
                 }
             }
             List<string> str = new List<string>();
-            for (int i=0;i<charArray.Length;i++)
+            for (int i = 0; i < charArray.Length; i++)
             {
                 str.Add(charArray[i].ToString());
             }
@@ -44,8 +44,37 @@ namespace First_Variation_on_Caesar_Cipher
         public static string demovingShift(List<string> s, int shift)
         {
             //the same as with movingShift but demoving shift
-            char[] demo
-            return "";
+            var temp = 0;
+            List<string> str = s.ToList();
+            char[] charArray = str.ToString().ToCharArray();
+            for (int i = 0; i < charArray.Length; i++)
+            {
+                if (shift >= 26)
+                    shift = shift - 26;
+
+                if (charArray[i] == ' ')
+                {
+                    shift++;
+                    continue;
+                }
+
+                if (char.IsLetter(charArray[i]))
+                {
+                    temp = Convert.ToInt32(charArray[i]) - shift;
+                    if (temp >= 123 && Convert.ToInt32(charArray[i]) > 97 || temp >= 91 && Convert.ToInt32(charArray[i]) <= 90)
+                        temp = temp - 26;
+                    charArray[i] = Convert.ToChar(temp);
+                    shift++;
+                }
+            }
+            List<string> demoved = new List<string>();
+            for (int i = 0; i < charArray.Length; i++)
+            {
+                demoved.Add(charArray[i].ToString());
+            }
+            string result = demoved.ToString();
+            Console.WriteLine(result);
+            return result;
         }
 
         static void Main(string[] args)
@@ -54,8 +83,8 @@ namespace First_Variation_on_Caesar_Cipher
             string s = Console.ReadLine();
             Console.WriteLine("Input, how many shifts to perform");
             int shift = int.Parse(Console.ReadLine());
-            Console.WriteLine("Shifted string: " + string.Join("",movingShift(s, shift)));
-            //Console.WriteLine("Demived shifting: " + demovingShift(s, shift));
+            Console.WriteLine("Shifted string: " + string.Join("", movingShift(s, shift)));
+            //Console.WriteLine("Demived shifting: " + demovingShift(s.ToList(), shift));
             Console.ReadLine();
         }
     }
