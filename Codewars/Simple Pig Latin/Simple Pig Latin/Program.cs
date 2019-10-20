04dd045ca22bfa5535f45e753a91b2d237253da9
Vladimir Krygin <nif315@gmail.com>
2019-10-20 13:47:04 +0300

    Simple pig latin kata added

---
+++ Codewars/Simple Pig Latin/Simple Pig Latin/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Simple_Pig_Latin
+{
+    class Program
+    {
+        public static string PigIt(string str)
+        {
+            string[] arr = str.Split(' ');
+            string tmp;
+            string firstLetter;
+            for(int i=0;i<arr.Length;i++)
+            {
+                firstLetter = arr[i].Substring(0, 1);
+                tmp = arr[i].Substring(1);
+                tmp += firstLetter + "ay";
+                arr[i] = tmp;
+            }
+            string result = string.Join(" ",arr);
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            Console.WriteLine(PigIt(str));
+            Console.ReadKey();
+        }
+    }
+}
